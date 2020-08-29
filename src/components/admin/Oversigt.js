0c3df6c3647b7713c9d7316fbed71a9419208d89
jepras <jeprasher@gmail.com>
2020-08-29 19:28:18 +0200

    commented app table

--- src/components/admin/Oversigt.js
+++ src/components/admin/Oversigt.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import { firestoreConnect } from 'react-redux-firebase';
 import { compose } from 'redux';
 import { decide } from '../../store/actions/adminActions';
-import ApplicationTable from './ApplicationTable';
+/* import ApplicationTable from './ApplicationTable'; */
 
 class Oversigt extends Component {
   constructor(props) {
